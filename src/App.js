a97fb35d7e677f58d99c880e2927d09abb268362
Glitch (hello-sqlite) <none>
2019-12-11 18:11:07 +0000

    💑🕵 Checkpoint ./src/App.js:9299062/12529

--- src/App.js
+++ src/App.js
@@ -2,10 +2,71 @@ import React from 'react';
 import logo from './sun.svg';
 import './App.css';
 
-function App() {
-  return (
-    <p>Testing first time on glitch</p>
+class App extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    this.state = {
+      weatherInfo: []
+    }
+  }
+  
+  componentWillMount() {
+    
+  }
+  
+  componentDidMount() {
+    
+  }
+  
+  render() {
+    return (
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="weather-card one">
+            <div class="top">
+              <div class="wrapper">
+                <div class="mynav">
+                  <a href="javascript:;"><span class="lnr lnr-chevron-left"></span></a>
+                  <a href="javascript:;"><span class="lnr lnr-cog"></span></a>
+                </div>
+                <h1 class="heading">Clear night</h1>
+                <h3 class="location">Dhaka, Bangladesh</h3>
+                <p class="temp">
+                  <span class="temp-value">20</span>
+                  <span class="deg">0</span>
+                  <a href="javascript:;"><span class="temp-type">C</span></a>
+                </p>
+              </div>
+            </div>
+            <div class="bottom">
+              <div class="wrapper">
+                <ul class="forecast">
+                  <a href="javascript:;"><span class="lnr lnr-chevron-up go-up"></span></a>
+                  <li class="active">
+                    <span class="date">Yesterday</span>
+                    <span class="lnr lnr-sun condition">
+                      <span class="temp">23<span class="deg">0</span><span class="temp-type">C</span></span>
+                    </span>
+                  </li>
+                  <li>
+                    <span class="date">Tomorrow</span>
+                    <span class="lnr lnr-cloud condition">
+                      <span class="temp">21<span class="deg">0</span><span class="temp-type">C</span></span>
+                    </span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
+  }
+  
+  
 }
 
 export default App;
